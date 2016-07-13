d7ec3c8670f664b361d7c3290d5fcac55be959b1
godL <547188371@qq.com>
2016-07-13 15:30:02 +0800

    Initial Commit

---
+++ LHDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHDB
+//
+//  Created by 李浩 on 16/7/13.
+//  Copyright © 2016年 李浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHDB
+//
+//  Created by 李浩 on 16/7/13.
+//  Copyright © 2016年 李浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
